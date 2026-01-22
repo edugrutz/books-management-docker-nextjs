95aa05e77d7ac625aa263b21ade2d7160aeb146f
Eduardo <grutzmann9@gmail.com>
2026-01-22 14:54:43 -0300

    feat(frontend): add basic initial page with grid book

---
+++ frontend/codes/src/lib/api.ts
@@ -0,0 +1,13 @@
+import { Book } from "@/types/book"
+
+export async function getBooks(): Promise<Book[]> {
+    const res = await fetch(`${process.env.API_URL}/api/v1/books`, {
+        cache: "no-store",
+    })
+
+    if (!res.ok) {
+        throw new Error("Erro ao buscar livros")
+    }
+
+    return res.json()
+}
